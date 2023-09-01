222d0931adcab12422d96c9c663fdc0fc9523a5a
Shaun Sundquist <5550411+shaunsund@users.noreply.github.com>
2023-09-01 18:27:05 -0400

    WIP missing modules

--- blink.py
+++ blink.py
@@ -1,10 +1,20 @@
 import time
+import rp2
 from machine import Pin
+import adafruit_dht
+# import sys
+
+# print("Python version")
+# print(sys.version)
+# print("Version info.")
+# print(sys.version_info)
 
 board_led = Pin("LED", Pin.OUT, value=1)
 small_led = Pin(21, Pin.OUT, value=1)
-red_led = Pin(9, Pin.OUT, value=1)
-green_led = Pin(10, Pin.OUT, value=1)
+lone_led = Pin(13, Pin.OUT, value=1)
+
+# dht_pin = Pin(22, Pin.IN, Pin.PULL_UP)
+# dht_sensor = adafruit_dht.DHT22(dht_pin)
 
 while True:
     board_led.toggle()
@@ -17,12 +27,15 @@ while True:
     small_led.toggle()
     time.sleep(0.5)
 
-    red_led.toggle()
+    lone_led.toggle()
     time.sleep(0.5)
-    red_led.toggle()
+    lone_led.toggle()
     time.sleep(0.5)
-
-    green_led.toggle()
-    time.sleep(0.5)
-    green_led.toggle()
-    time.sleep(0.5)
+    # try:
+    #     temperature = dht_sensor.temperature
+    #     humidity = dht_sensor.humidity
+    #     print(f"Temperature: {temperature} °C")
+    #     print(f"Humidity: {humidity}%")
+    # except RuntimeError as e:
+    #     print("Error reading from AM2302: ", str(e))
+    #     time.sleep(2.0)
